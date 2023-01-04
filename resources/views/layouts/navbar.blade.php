183eaf546d14cb9d744769c3aa08dbb6a8748173
zakarya3 <zakaria.aanni@gmail.com>
2023-01-04 10:04:52 +0100

    update

--- resources/views/layouts/navbar.blade.php
+++ resources/views/layouts/navbar.blade.php
@@ -4,7 +4,7 @@
 
 <!-- Mirrored from codescandy.com/coach/ by HTTrack Website Copier/3.x [XR&CO'2014], Sun, 25 Dec 2022 17:34:51 GMT -->
 <head>
-    <title>Coach - Fatima Zekhnini</title>
+    <title>Coach - Faima Zekhnini</title>
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
